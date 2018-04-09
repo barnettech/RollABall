8ce7be353401aca517d1935db8640dd1d42415a7
James Barnett <barnettech@gmail.com>
2018-04-08 21:25:51 -0400

    first commit

---
+++ Assets/GroundSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundSpawner : MonoBehaviour {
+
+	public GameObject[] prefabs;
+    public float spawnDistance;
+    private GameObject playerLastPosition;
+    private GameObject playerPosition;
+    float distanceTravelled = 0;
+    Vector3 lastPosition;
+    
+	// Use this for initialization
+	void Start () {
+
+		// infinite coin spawning function, asynchronous
+		//StartCoroutine(SpawnGround());
+        playerPosition = GameObject.Find("Player");
+        lastPosition = playerPosition.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+      distanceTravelled += Vector3.Distance(playerPosition.transform.position, lastPosition);
+      lastPosition = playerPosition.transform.position;
+      // Debug.Log("distanceTravelled is " + distanceTravelled);
+        
+      
+    }
+
+}
+
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public GameObject[] prefabs;
+    
+    public float speed;
+    public Vector3 jump;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+    }
+    
+    void Update() {
+      if (Input.GetButtonDown("Jump")) {
+         print("space key was pressed");
+         rb.AddForce(jump * 2.0f, ForceMode.Impulse);
+      }
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ( "Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+    
+}
