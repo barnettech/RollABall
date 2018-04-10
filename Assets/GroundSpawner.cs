879d6dee49967c5af1e43a2482d7add1e42c9b78
James Barnett <barnettech@gmail.com>
2018-04-09 21:58:49 -0400

    removing ground when behind the player, needs some work

--- Assets/GroundSpawner.cs
+++ Assets/GroundSpawner.cs
@@ -11,6 +11,7 @@ public class GroundSpawner : MonoBehaviour {
     float distanceTravelled = 0;
     Vector3 lastPosition;
     private bool flag = false;
+    int tileCount = 0;
     
 	// Use this for initialization
 	void Start () {
@@ -26,15 +27,27 @@ public class GroundSpawner : MonoBehaviour {
 	void Update () {
       distanceTravelled += Vector3.Distance(playerPosition.transform.position, lastPosition);
       lastPosition = playerPosition.transform.position;
-      Debug.Log("distanceTravelled is " + distanceTravelled);
-      if(distanceTravelled > .5 && flag == false) {
+      // Debug.Log("tileCount is " + tileCount + " and distanceTravelled is " + distanceTravelled);
+      if(distanceTravelled > 20 * tileCount ) {
+          tileCount++;
           GameObject clone;
-          clone = Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(groundPosition.transform.position.x, groundPosition.transform.position.y, groundPosition.transform.position.z + 48), Quaternion.identity);
+          clone = Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(groundPosition.transform.position.x, groundPosition.transform.position.y, groundPosition.transform.position.z + 48 * tileCount), Quaternion.identity);
           clone.transform.rotation = groundPosition.transform.rotation;
-          flag = true;
+          // flag = true;
       }
         
-      
+      GameObject[] argo = GameObject.FindGameObjectsWithTag("Ground");
+ foreach (GameObject go in argo) {
+   Debug.Log("tagged position is " + go.transform.position.z);
+   Debug.Log("player position is " + playerPosition.transform.position.z);
+   if(go.transform.position.z < playerPosition.transform.position.z + 5) {
+     Debug.Log("in here");
+     Destroy(go);
+   }
+   // Debug.Log(go.name);
+ }
+        
+        
     }
 
 }
