b8ac6d91593d8084e0c4222c096b92240646c869
James Barnett <barnettech@gmail.com>
2018-04-19 14:19:53 -0400

    thirdpersoncharacter looking great, and everything is now wired up again, follow camera, scoring etc

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -14,6 +14,14 @@ public class CameraController : MonoBehaviour {
     
     void LateUpdate ()
     {
-        transform.position = player.transform.position + offset;
+        //transform.position = player.transform.position + offset;
+        
+        
+        GameObject playerCurrent = GameObject.FindGameObjectWithTag ("Player");
+             Vector3 currentPlayerPosition = playerCurrent.transform.position;
+             transform.position = new Vector3 (currentPlayerPosition.x, currentPlayerPosition.y + 2.0f, currentPlayerPosition.z - 3.0f);
+             Vector3 lookAtPos = currentPlayerPosition;
+             lookAtPos.y = lookAtPos.y + 1.0f;
+             transform.LookAt (lookAtPos);
     }
 }
