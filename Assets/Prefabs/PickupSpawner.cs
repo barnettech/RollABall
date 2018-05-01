eb96bef03e39f67bd7e49ae08837fcf88df9bf1f
James Barnett <barnettech@gmail.com>
2018-04-30 23:54:04 -0400

    gets the coins working with the incline

--- Assets/Prefabs/PickupSpawner.cs
+++ Assets/Prefabs/PickupSpawner.cs
@@ -32,7 +32,7 @@ public class PickupSpawner : MonoBehaviour {
       // Debug.Log("tileCount is " + tileCount + " and distanceTravelled is " + distanceTravelled);
       if(distanceTravelled > 10 * tileCount ) {
           tileCount++;
-          GameObject activePrefab = Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(groundPosition.transform.position.x, groundPosition.transform.position.y, groundPosition.transform.position.z + 48 * tileCount), Quaternion.identity) as GameObject;
+          GameObject activePrefab = Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(groundPosition.transform.position.x, groundPosition.transform.position.y - (30 * tileCount), groundPosition.transform.position.z + 48 * tileCount), Quaternion.identity) as GameObject;
           activePrefab.transform.rotation = groundPosition.transform.rotation;
           pickuplist.Add(activePrefab);
           GameObject gameObjectToRemove1 = pickuplist[1];
